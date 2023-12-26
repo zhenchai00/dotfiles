ed9acbd6ded5a5ecd6397c3b2bb90c1cd220dd86
caizhen <zhenchai0000@gmail.com>
2023-12-26 20:53:36 +0800

    update with yes command

--- linuxmachine.sh
+++ linuxmachine.sh
@@ -4,7 +4,7 @@
 sudo apt update -y && sudo apt upgrade -y
 
 # Install packages
-sudo apt install neovim git
+sudo apt install neovim git -y
 
 # Configure git
 git config --global user.email "zhenchai0000@gmail.com"
@@ -20,11 +20,11 @@ else
     echo "Installing Docker ......."
     # Uninstall old versions docker
     for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
-        sudo apt-get remove $pkg;
+        sudo apt-get remove $pkg -y;
     done
     # Add Docker's official GPG key:
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl gnupg
+    sudo apt-get update -y
+    sudo apt-get install ca-certificates curl gnupg -y
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -34,9 +34,9 @@ else
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
+    sudo apt-get update -y
 
     # Install Docker
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 fi
 
