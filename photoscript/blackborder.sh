da47d783b77998934dda8bebfad4c8dfa28138ee
zhenchai <zhenchai0000@gmail.com>
2023-12-26 16:19:50 +0800

    add: blackborder.sh for photo

---
+++ photoscript/blackborder.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Add white border to make square image, default to apply white border to all jpg found within current directory
+
+# Convert all image file in the folder
+for file in "$PWD/*"; do
+    convert $file -set filename:f "%[t]_sq_border" -resize 1040x1040 -background black -gravity center -extent 1080x1080 -quality 100 "%[filename:f].jpg"
+    echo $file
+done
