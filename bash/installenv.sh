d76fb81cb6ca6745cf69905b4ebd609bb9df85f7
caizhen <zhenchai0000@gmail.com>
2023-12-28 15:57:59 +0800

    update: gpg confirm overwrite file

--- bash/installenv.sh
+++ bash/installenv.sh
@@ -57,7 +57,7 @@ else
     sudo apt-get update -y
     sudo apt-get install ca-certificates curl gnupg -y
     sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
     # Add the repository to Apt sources:
